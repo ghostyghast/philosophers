271eb22c96b333a2189e230f7367ef67b336c461
ghostyghast <antoinethebestgamer@gmail.com>
2023-08-30 14:42:23 +0800

    test

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 16:09:38 by amaligno          #+#    #+#             */
-/*   Updated: 2023/08/29 18:11:35 by amaligno         ###   ########.fr       */
+/*   Updated: 2023/08/29 18:11:57 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ u_int64_t	ft_gettime(void)
 void	ft_usleep(u_int64_t time)
 {
 	u_int64_t		start;
-	u_int64_t		wait;
 
-	wait = time;
 	start = ft_gettime();
 	while ((ft_gettime() - start) < time)
 		usleep(time / 10);
